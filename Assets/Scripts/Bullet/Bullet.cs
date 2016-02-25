ae70399609285fedb2f0d6d6726a52184c6b1190
HaiNguyen <mr.haicaca@gmail.com>
2016-02-26 00:12:08 +0700

    push all

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	// 弾の移動スピード
+	public int speed = 10;
+
+	// ゲームオブジェクト生成から削除するまでの時間
+	public float lifeTime = 1;
+
+	// 攻撃力
+	public int power = 1;
+
+	void Start ()
+	{
+		// ローカル座標のY軸方向に移動する
+		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+		
+		// lifeTime秒後に削除
+		Destroy (gameObject, lifeTime);
+	}
+	void OnTriggerEnter2D(Collider2D c){
+		string layerName = LayerMask.LayerToName(c.gameObject.layer);
+		if( layerName == "Player")
+		{
+			// 弾の削除
+			Destroy(c.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	// Spaceshipコンポーネント
+	Spaceship spaceship;
+	float swipeSpeed;
+	//Touch touch;
+	Vector2 prevMousePos;
+	Vector2 currentMousePos;
+
+	IEnumerator Start ()
+	{
+		// Spaceshipコンポーネントを取得
+		spaceship = GetComponent<Spaceship> ();
+		prevMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		while (true) {
+			
+			// 弾をプレイヤーと同じ位置/角度で作成
+			if(Input.GetMouseButton(0)){
+				spaceship.Shot (transform);
+			}
+
+			// ショット音を鳴らす
+			GetComponent<AudioSource>().Play();
+			
+			// shotDelay秒待つ
+			yield return new WaitForSeconds (spaceship.shotDelay);
+		}
+	}
+		void Update ()
+	{   
+		prevMousePos = currentMousePos;
+		currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		float swipeSpeed = (currentMousePos - prevMousePos).magnitude / Time.deltaTime;
+	
+		Vector2 direction = (currentMousePos - prevMousePos).normalized;
+
+		// 移動の制限
+
+		if (Input.GetMouseButton (0)){
+			Move (direction, swipeSpeed);
+		}
+		
+	}
+
+	// 機体の移動
+	void Move (Vector2 direction, float swipeSpeed)
+	{
+		// 画面左下のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+		
+		// 画面右上のワールド座標をビューポートから取得
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+		
+		// プレイヤーの座標を取得
+		Vector2 pos = transform.position;
+		//Debug.Log (pos);
+		// 移動量を加える
+		pos += direction  * spaceship.speed * swipeSpeed * (Time.deltaTime);
+		//Debug.Log ("Time" + Time.deltaTime	);
+		// プレイヤーの位置が画面内に収まるように制限をかける
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+		// 制限をかけた値をプレイヤーの位置とする
+		transform.position = pos;
+	}
+	
+	// ぶつかった瞬間に呼び出される
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		// レイヤー名を取得
+		string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+		// レイヤー名がBullet (Enemy)またはEnemyの場合は爆発
+		if( layerName == "Bullet (Enemy)" || layerName == "Enemy")
+		{
+			// Managerコンポーネントをシーン内から探して取得し、GameOverメソッドを呼び出す
+			FindObjectOfType<Manager>().GameOver();
+
+			// 爆発する
+			spaceship.Explosion();
+		
+			// プレイヤーを削除
+			Destroy (gameObject);
+		}
+	}
+}
